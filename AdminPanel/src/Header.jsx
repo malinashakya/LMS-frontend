596e6d959edea19caaaadaf2b8839e01144f38d2
Malina <malinashakya72@gmail.com>
2024-03-25 00:13:13 +0545

    Added form frontend views

--- AdminPanel/src/Header.jsx
+++ AdminPanel/src/Header.jsx
@@ -1,24 +1,49 @@
+import { useState } from "react";
 import {
   BsFillBellFill,
-  BsFillEnvelopeFill,
   BsPersonCircle,
   BsSearch,
   BsJustify,
 } from "react-icons/bs";
 
 function Header({ OpenSidebar }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleLogout = () => {
+    // Perform logout logic here, such as clearing authentication tokens, etc.
+    // Then redirect to the login page
+    window.location.href = "http://localhost:8084/login";
+  };
+
   return (
     <header className="header">
+      <div className="logo-container">
+        <h1 className="logo">HRMS</h1>
+      </div>
       <div className="menu-icon">
         <BsJustify className="icon" onClick={OpenSidebar} />
       </div>
       <div className="header-left">
-        <BsSearch className="icon" />
+        <div className="search-container">
+          <input type="text" placeholder="Search..." />
+          <BsSearch className="search-icon" style={{ color: "grey" }} />
+        </div>
       </div>
       <div className="header-right">
         <BsFillBellFill className="icon" />
-        <BsFillEnvelopeFill className="icon" />
-        <BsPersonCircle className="icon" />
+        <div className="dropdown-container">
+          <BsPersonCircle
+            className="icon"
+            onClick={() => setShowDropdown(!showDropdown)}
+          />
+          {showDropdown && (
+            <div className="dropdown">
+              <ul>
+                <li onClick={handleLogout}>Logout</li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
